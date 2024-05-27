f802487e5dc0d2fb27d50382019009395beb1f97
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-27 20:53:56 +0530

    ddddddas

---
+++ js/01_chapter.js
@@ -0,0 +1,39 @@
+console.log("Gulshan Bauddh GB");0.
+var a=5; // "var" is "globle" varaible;
+var b=6;
+var c=a+b;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(typeof c);
+
+let aa=5; // "let" is "Local" varaible;
+let bb=6;
+let cc=a+b;
+console.log(aa);
+console.log(bb);
+console.log(cc);
+console.log(typeof cc);
+
+let gul ={
+    name:"Gulshan Bauddh",
+    rollnum: 19,
+    mark:102
+}
+console.log(gul);
+
+// ======= Practice - 1 =======
+// === 1 ===
+
+let str = "Gulshan";
+console.log(str);
+let str2 = str+2;
+console.log(str2);
+// === 2 ===
+console.log(typeof str);
+console.log(typeof str2);
+// === 3 ===
+
+
+
+
