e774a5350f7b3726ed84b66a19bba932e6b4c692
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-27 21:27:23 +0530

    yyygy

---
+++ js/02_chapter.js
@@ -0,0 +1,38 @@
+let a = 8;
+let b = 2;
+// Arithmatic Operators
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=",a*b);
+// console.log("a**b=",a**b);
+// console.log("a/b=",a/b);
+// console.log("a%b=",a%b);
+// console.log(a++);
+// console.log("a++=",a);
+// console.log(a--);
+// console.log("a--=",a);
+// console.log("Assignment Operators")
+// Assignment Operators
+
+// let x=5
+// let y=9
+// y=7;
+// console.log("Current value of y is=",y)
+
+// let x=5
+// let y=9
+// x+=y;
+// let x=5
+// let y=9
+// x-=y;
+// console.log("Current value of x is=",x);
+
+// if (5 == 25) {
+//   console.log(true);
+// } else console.log(false);
+
+if (25 != 25) {
+  console.log(true);
+} else if(6==6) 
+    console.log("false1");
+else console.log("true908908")
