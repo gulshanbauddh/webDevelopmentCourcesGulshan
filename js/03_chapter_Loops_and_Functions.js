51c89b24ebe55cc26c1a8b601e62a130e8ed9464
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-28 12:38:12 +0530

    hhh

---
+++ js/03_chapter_Loops_and_Functions.js
@@ -0,0 +1,10 @@
+//  Loops and Functions ( Chapter -3 )
+// #1 for loop
+for(i=1; i<=9; i++){
+    console.log("Gulshan Bauddh",i)
+}
+
+// #2 for-in loop
+// for in loop work in only Array
+
+// #3 for-of loop
