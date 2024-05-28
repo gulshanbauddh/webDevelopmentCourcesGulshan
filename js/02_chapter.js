18c42322c535b4f814b862a55ad1ded352870a42
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-28 07:47:12 +0530

    Chapter 2 Completed

--- js/02_chapter.js
+++ js/02_chapter.js
@@ -1,5 +1,5 @@
-let a = 8;
-let b = 2;
+// let a = 8;
+// let b = 2;
 // Arithmatic Operators
 // console.log("a+b=",a+b);
 // console.log("a-b=",a-b);
@@ -43,3 +43,38 @@ let b = 2;
 
 // ==== Practice set 2 ====
 
+// === Q-1===
+// let a=11;
+// if(a>=10 && a<=20){
+//     console.log("Age between 10 and 20");
+// }
+
+// === Q-2===
+// let a = 2;
+// switch (a) {
+    //   case 1:
+    //     console.log("This is 1st.");
+    //     break;
+    //   case 2:
+    //     console.log("This is 2nd.");
+    //     break;
+    //   case 3:
+    //     console.log("This is 3rd");
+    //   default:
+    //     break;
+    // }
+// === Q-3 ===
+// let a=11;
+// if(a%2==0){
+//     console.log("This number is Divisible by 2");
+// } else if (a%3==0){
+//     console.log("This number is Divisible by 3");
+// }else console.log("This number is not Divisible 2 or 3");
+
+// === Q-4 ===
+// let a=6;
+// a%2==0 ? console.log("Divisible by 2") : console.log("Divisible by 3");
+
+// === Q-5 ===
+// let a=18;
+// (a>=18) ? console.log("You can Drive.") : console.log("You can't Drive");
