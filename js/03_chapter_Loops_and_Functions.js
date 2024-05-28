b0d57fe653c2081c6b1f31d9e79d583e499c1efe
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-28 17:22:35 +0530

    jio

--- js/03_chapter_Loops_and_Functions.js
+++ js/03_chapter_Loops_and_Functions.js
@@ -1,10 +1,28 @@
 //  Loops and Functions ( Chapter -3 )
 // #1 for loop
-for(i=1; i<=9; i++){
-    console.log("Gulshan Bauddh",i)
-}
+// for(i=1; i<=5; i++){
+//     console.log("Gulshan Bauddh",i)
+// }
 
 // #2 for-in loop
-// for in loop work in only Array
-
+// for-in loop work in only Objet
+let obj = {
+  name: Gulshan,
+  surName: Bauddh,
+  rollno: 12,
+  class: cse,
+};
+for (const key in obj) {
+  console.log(key);
+}
 // #3 for-of loop
+// for-of loop work in only Array
+
+// #4 While loop
+// let i=1;
+// while(i<=3){
+//     console.log("Gulshan Bauddh",i);
+//     i++;
+// }
+
+// ===== Function in JavaScript =====
