82c5d4e791593ce0270510b7dbedd11673e8ac13
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-28 18:09:28 +0530

    jio

--- js/03_chapter_Loops_and_Functions.js
+++ js/03_chapter_Loops_and_Functions.js
@@ -7,14 +7,21 @@
 // #2 for-in loop
 // for-in loop work in only Objet
 let obj = {
-  name: Gulshan,
-  surName: Bauddh,
-  rollno: 12,
-  class: cse,
+  name: "Gulshan",
+  surName: "Bauddh",
+  rollno: "12",
+  class: "cse",
 };
-for (const key in obj) {
-  console.log(key);
-}
+// for (const key in obj) {
+//     const element=obj[key];
+//   console.log(key, "->", element);
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key,element);
+//     }
+// }
 // #3 for-of loop
 // for-of loop work in only Array
 
@@ -26,3 +33,31 @@ for (const key in obj) {
 // }
 
 // ===== Function in JavaScript =====
+
+// console.log("Gulshan Bauddh");
+// function santance(hero) {
+//     console.log("hero! " +hero+ " is very wonderfull persion.");
+//     console.log("hero! " +hero+ " is very smart persion.");
+//     console.log(hero+ " is very good looking persion.");
+//     console.log(hero+ " is very wonderfull persion.");
+//     console.log(hero+ " is very powerfull persion.");
+// }
+// santance ("Gulshan");
+// santance("Ritik");
+
+// function add(a,b) {
+//     console.log("Addition is ",a+b);
+// }
+
+// add(7,9);
+// add(7,7);
+
+// function add(a,b) {
+//     return a+b;
+// }
+
+// prin1= add(6,8);
+// prin2= add(16,28);
+// console.log(prin1);
+// console.log(prin2);
+
