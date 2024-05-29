5e309f3087dfdd590268bb2946703ed963627105
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-29 14:11:03 +0530

    jio

--- js/05_Arrays.js
+++ js/05_Arrays.js
@@ -1,6 +1,39 @@
 const fruits=["Banana","Appel","Papaya","Grapes"];
-const a1=[9,5,"gulshan","nepali","kajal","arpita","ankita"];
+const a1=["kajal",9,5,"ankita","yasu"];
 
 // console.log(a1[3]);
 // console.log(a1.length);
-// console.log(a1.slice(2,5));
+// console.log(a1.slice(2,3));
+// console.log(a1.slice(2,3));
+// console.log(typeof a1);
+// console.log(a1)
+// a1[3]="Bauddh";
+// console.log(a1.toString());
+// console.log(a1.join("-"));
+// console.log(a1.join("+"));
+// console.log(a1.join(".A."));
+// console.log(a1.join("*"));
+
+// console.log("Gulshan",a1.pop(0));
+// console.log(a1.push(0));
+// console.log(a1.shift());
+// console.log(a1.unshift(54));
+// console.log(delete a1[5]);
+// console.log(a1.concat(fruits));
+
+// ***** Importent Sorting *****
+// console.log(a1.sort());
+// let a2=a1.sort();
+console.log(a1);
+
+// // #10 splice
+// let a3=a2.splice(3,1,"New","New","New","New");
+// console.log(a3);
+// console.log(a2);
+
+// #11 slice
+//console.log(a1.slice(3));
+// console.log(a1.slice(2,3));
+
+// #12 reverse
+console.log(a1.reverse());
