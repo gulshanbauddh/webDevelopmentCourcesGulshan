582b9e8f4c20d7c8319a65bb3fecf8b1400ca830
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-29 19:11:55 +0530

    jio

---
+++ js/Faulty_Calculator.js
@@ -0,0 +1,14 @@
+let no1 = 10;
+let no2 = 20;
+
+let a = Math.floor(Math.random() * 10); //Random Number.
+// console.log(a);
+if (a == 0) {
+  console.log("Gulshan 1");
+  calculator(5, 8);
+} else {
+  console.log("Gulshan 2");
+}
+function calculator(x, y) {
+  console.log(a + b);
+}
