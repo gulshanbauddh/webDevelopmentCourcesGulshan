2d3c82660d62f1af5a2724fb36774c60599cfd25
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-29 20:28:46 +0530

    jio

--- js/Faulty_Calculator.js
+++ js/Faulty_Calculator.js
@@ -2,20 +2,20 @@
 let a = Math.floor(Math.random() * 10);
 // console.log("Randan Number is:", a);
 
-let no = 4;
-let no1 = 10;
-let no2 = 20;
+let no = 4;     // 1:for=>Addition, 2:for=>Substraction, 3:for=>Multipli, 4:for=>Divid. 
+let no1 = 10;   // 1st number;
+let no2 = 20;   // 2nd number;
 
 if (a != 0) {
   console.log("Right Calculator"); // This line Only for Testing purpase
   calculat(no, no1, no2);
 } else if (no != 4) {
   no = no + 1;
-  console.log("Wrong Calculatir"); // This line Only for Testing purpase
+  console.log("Faulty Calculatir"); // This line Only for Testing purpase
   calculat(no, no1, no2);
 } else {
   no = 1;
-  console.log("Wrong Calculatir"); // This line Only for Testing purpase
+  console.log("Faulty Calculatir"); // This line Only for Testing purpase
   calculat(no, no1, no2);
 }
 
