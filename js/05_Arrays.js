7307f97ef5f9f59ad08a6acb242bc1df548e7338
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-29 11:36:05 +0530

    JIO

---
+++ js/05_Arrays.js
@@ -0,0 +1,6 @@
+const fruits=["Banana","Appel","Papaya","Grapes"];
+const a1=[9,5,"gulshan","nepali","kajal","arpita","ankita"];
+
+// console.log(a1[3]);
+// console.log(a1.length);
+// console.log(a1.slice(2,5));
