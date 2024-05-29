078288ff73e8439997e61f71ec62323b07fea595
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-29 22:15:54 +0530

    jio

--- js/Faulty_Calculator.js
+++ js/Faulty_Calculator.js
@@ -2,23 +2,24 @@
 let a = Math.floor(Math.random() * 10);
 // console.log("Randan Number is:", a);
 
-let no = 4;     // 1:for=>Addition, 2:for=>Substraction, 3:for=>Multipli, 4:for=>Divid. 
-let no1 = 10;   // 1st number;
-let no2 = 20;   // 2nd number;
+const no = 1; // 1:for=>Addition, 2:for=>Substraction, 3:for=>Multipli, 4:for=>Divid.
+const no1 = prompt("Enter 1st number"); // 1st number;
+const no2 = prompt("Enter 2st number"); // 2nd number;
 
 if (a != 0) {
   console.log("Right Calculator"); // This line Only for Testing purpase
   calculat(no, no1, no2);
-} else if (no != 4) {
-  no = no + 1;
-  console.log("Faulty Calculatir"); // This line Only for Testing purpase
-  calculat(no, no1, no2);
 } else {
-  no = 1;
-  console.log("Faulty Calculatir"); // This line Only for Testing purpase
-  calculat(no, no1, no2);
+  if (no != 4) {
+    no = no + 1;
+    console.log("Faulty Calculatir"); // This line Only for Testing purpase
+    calculat(no, no1, no2);
+  } else {
+    no = 1;
+    console.log("Faulty Calculatir"); // This line Only for Testing purpase
+    calculat(no, no1, no2);
+  }
 }
-
 function calculat(no, x, y) {
   switch (no) {
     case 1:
