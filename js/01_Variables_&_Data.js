6c86d4e0740522f50fff0ab7bf7709445a51c459
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-29 20:40:58 +0530

    jio

--- js/01_Variables_&_Data.js
+++ js/01_Variables_&_Data.js
@@ -1,25 +1,26 @@
-console.log("Gulshan Bauddh GB");0.
-var a=5; // "var" is "globle" varaible;
-var b=6;
-var c=a+b;
+console.log("Gulshan Bauddh GB");
+0;
+var a = 5; // "var" is "globle" varaible;
+var b = 6;
+var c = a + b;
 console.log(a);
 console.log(b);
 console.log(c);
 console.log(typeof c);
 
-let aa=5; // "let" is "Local" varaible;
-let bb=6;
-let cc=a+b;
+let aa = 5; // "let" is "Local" varaible;
+let bb = 6;
+let cc = a + b;
 console.log(aa);
 console.log(bb);
 console.log(cc);
 console.log(typeof cc);
 
-let gul ={
-    name:"Gulshan Bauddh",
-    rollnum: 19,
-    mark:102
-}
+let gul = {
+  name: "Gulshan Bauddh",
+  rollnum: 19,
+  mark: 102,
+};
 console.log(gul);
 
 // ======= Practice - 1 =======
@@ -27,13 +28,9 @@ console.log(gul);
 
 let str = "Gulshan";
 console.log(str);
-let str2 = str+2;
+let str2 = str + 2;
 console.log(str2);
 // === 2 ===
 console.log(typeof str);
 console.log(typeof str2);
 // === 3 ===
-
-
-
-
