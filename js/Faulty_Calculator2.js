b35601d93f449f55954dfee0d32c59de559549f6
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-30 16:34:14 +0530

    jio

--- js/Faulty_Calculator2.js
+++ js/Faulty_Calculator2.js
@@ -1,14 +1,10 @@
-// Faulty Calculator
+// Faulty Calculator give 10% Wrong Answer
 let random = Math.random();
 console.log(random);
 
 no1=prompt("Enter 1st Number")
 no=prompt("Enter Operation")
 no2=prompt("Enter 2st Number")
-
-// no1 = 6;
-// no2 = 4;
-// no = "+";
 
 let obj = {
   "+": "-",
@@ -18,14 +14,12 @@ let obj = {
 };
 
 // Rigth Calculation
-if (random < 0.1) {
-    alert("I am Right");
-  alert(`The result is ${eval(`${no1} ${no} ${no2}`)}`);
+if (random > 0.2) {
+  alert(`The Right result is: ${eval(`${no1} ${no} ${no2}`)}`);
 }
 
 // Wrong Calculation
 else {
   no=obj[no];
-  alert(`The result is ${eval(`${no1} ${no} ${no2}`)}`);
-  alert("I am Wrong");
+  alert(`The Wrong result is: ${eval(`${no1} ${no} ${no2}`)}`);
 }
