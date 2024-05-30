823564678fc1d57308e885f6a11440cc5efdcdbe
gulshanbauddh <gulshanbncet@gmail.com>
2024-05-30 18:03:58 +0530

    jio

--- js/05_Arrays.js
+++ js/05_Arrays.js
@@ -1,12 +1,13 @@
 const fruits=["Banana","Appel","Papaya","Grapes"];
 const a1=["kajal",9,5,"ankita","yasu"];
+const a2=["kajal2",92,52,"ankita2","yasu2"];
 
 // console.log(a1[3]);
 // console.log(a1.length);
 // console.log(a1.slice(2,3));
 // console.log(a1.slice(2,3));
 // console.log(typeof a1);
-// console.log(a1)
+// console.log(a1);
 // a1[3]="Bauddh";
 // console.log(a1.toString());
 // console.log(a1.join("-"));
@@ -14,17 +15,19 @@ const a1=["kajal",9,5,"ankita","yasu"];
 // console.log(a1.join(".A."));
 // console.log(a1.join("*"));
 
-// console.log("Gulshan",a1.pop(0));
-// console.log(a1.push(0));
+// console.log("Gulshan",a1.pop(4));
+// console.log(a1.push(25));
+// console.log(a1);
 // console.log(a1.shift());
 // console.log(a1.unshift(54));
 // console.log(delete a1[5]);
-// console.log(a1.concat(fruits));
+// console.log(a1.concat(fruits,a2));
+// console.log(a1);
 
 // ***** Importent Sorting *****
 // console.log(a1.sort());
 // let a2=a1.sort();
-console.log(a1);
+// console.log(a1);
 
 // // #10 splice
 // let a3=a2.splice(3,1,"New","New","New","New");
@@ -36,5 +39,25 @@ console.log(a1);
 // console.log(a1.slice(2,3));
 
 // #12 reverse
-console.log(a1.reverse());
+// console.log(a1.reverse());
 
+// Practice Set:5
+// === Q:1 ===
+// let ar=[1,2];
+// console.log(ar);
+// let arNew=prompt("Enter Arrays new number"); // "prompt" Look in This Line.
+// ar.push(arNew);
+// alert(`New Array: [${ar}]`);
+
+// === Q:3 ===
+// let ar=[20,40,44,90,76,11,30,110,76];
+// console.log("Full Array", ar);
+// const arN=[];
+// ar.forEach(element => {
+//     if(element%10==0){
+//         arN.push(element);
+//     }
+// });
+// console.log("Array which is divisible by 10 :", arN);
+
+// === Q:4===
