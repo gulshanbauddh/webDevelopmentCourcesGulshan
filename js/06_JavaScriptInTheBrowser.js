a7bcc056b6ac94ab23a135b6265615aafe410f38
gulshanbauddh <gulshanbncet@gmail.com>
2024-06-09 15:56:45 +0530

    jio

---
+++ js/06_JavaScriptInTheBrowser.js
@@ -0,0 +1,30 @@
+// alert("Gulshan Bauddh")
+// let x = 5;
+// let y = 5;
+// console.assert(x + y == 11, "Expression returned 'false'");
+// let xx = 5;
+// let yy = 6;
+// console.assert(xx + yy == 11, "Expression returned 'false'");
+// console.table("Gulshan_1");
+// console.warn("Gulshan_2");
+// console.error("Gulshan_3");
+// console.clear();  // Due to This line of code cleare all of above statement frome console
+// console.info("Gulshan_4");
+// console.assert("Gulshan_5");
+
+// Practice set-6
+// Question-1
+// let age = prompt("Enter your age.");
+// if (age >= 18) {
+//     alert("You are Drive");
+// } else alert("You are not Drive");
+
+// Question-2
+// let a=confirm("You age is over 18.")
+// if(a==true){
+//     alert("You can Drive");
+// }else alert("You not Drive");
+
+// Question-5
+// const bg=prompt("Enter Backgroung colour.");
+// document.body.style.background=bg;
